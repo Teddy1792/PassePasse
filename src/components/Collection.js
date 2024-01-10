9e2a7945e8017c64769a55ff5fb3b0d4c88ba451
Theodore Abitbol <theodore.abitbol@gmail.com>
2024-01-10 15:54:48 +0100

    added book descriptions

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import '../styles/Collection.scss';
+import { Modal } from './Modal';
 import Naissance from '../img/photos couvertures/Naissance.jpg';
 import TuNeTaderasGuere from '../img/photos couvertures/Tu ne tarderas guère.png';
-import { Modal } from './Modal';
+import NaissanceData from '../textes/Naissance.json';
+import TuNeTarderas from '../textes/TuNeTarderasGuere.json';
+import '../styles/Collection.scss';
 
 export function Collection() {
     const [modalLink, setModalLink] = useState('');
@@ -14,33 +16,46 @@ export function Collection() {
 
     const handleButtonClick = (title, text, image, link) => {
         setModalTitle(title);
-        setModalContent(text);
+        setModalContent(text); // Set the text content directly
         setModalImage(image);
         setModalLink(link);
         setIsFading(false);
         setModalOpen(true);
     };
     
-
     const closeModal = () => {
         setIsFading(true);
-            setModalOpen(false);
+        setModalOpen(false);
     };
 
     return (
         <nav>
             <div className='backgroundImgCollection'></div>
             <div className='booksCover'>
-                <div className='naissanceBox'>
-                <button onClick={() => handleButtonClick('Naissance', 'Récit de naissance', Naissance, 'https://buy.stripe.com/test_00g3fl9UfcdOeNW3cd')}><p>Naissance</p></button>
-                </div>
+            <div className='naissanceBox'>
+    <button onClick={() => handleButtonClick(
+        "Naissance", 
+        NaissanceData.description.split('\n').map((line, index) => (
+            <p key={index}>{line}</p>
+        )),
+        Naissance,
+        'https://buy.stripe.com/test_00g3fl9UfcdOeNW3cd'
+    )}>
+        <p>Naissance</p>
+    </button>
+</div>
                 <div className='confessionBox'>
-                <button onClick={() => handleButtonClick("Tu ne tarderas guère", 'Text for Confession', TuNeTaderasGuere, 'https://buy.stripe.com/test_bIYcPV2rN1zafS0eUU')}><p>Tu ne tarderas guère</p></button>
+                    <button onClick={() => handleButtonClick(
+                        "Tu ne tarderas guère", 
+                        TuNeTarderas.description.split('\n').map((line, index) => (
+                            <p key={index}>{line}</p>
+                        )),
+                        TuNeTaderasGuere, 
+                        'https://buy.stripe.com/test_bIYcPV2rN1zafS0eUU'
+                    )}><p>Tu ne tarderas guère</p></button>
                 </div>
             </div>
             <Modal title={modalTitle} text={modalContent} image={modalImage} link={modalLink} closeModal={closeModal} fading={isFading} />
         </nav>
     );
 }
-
-//Livres = formerly Collection, hence the divergence between the component name Collection and the route name!
